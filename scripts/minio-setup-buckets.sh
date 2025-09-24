3e3ed9f7ab841dc7ac057e6bfe1bc87f9fb6403d
epickaowca <epickaowca@Epickas-Laptop.local>
2025-09-24 13:34:34 +0200

    minio-init quality job setup

--- scripts/minio-setup-buckets.sh
+++ scripts/minio-setup-buckets.sh
@@ -2,7 +2,6 @@
 
 /usr/bin/mc alias set minioalias http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 /usr/bin/mc mb --ignore-existing minioalias/main
-# /usr/bin/mc anonymous set download minioalias/main
 /usr/bin/mc admin user add minioalias ${MINIO_USER} ${MINIO_PASSWORD}
 /usr/bin/mc admin policy attach minioalias readwrite --user ${MINIO_USER}
 

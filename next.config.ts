1d666e7c38fa577760224a54f063f9d8298fc9ab
epickaowca <epickaowca@Epickas-Laptop.local>
2025-09-24 13:09:44 +0200

    minio-init next image domain config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'standalone',
     images: {
-        domains: ['localhost'],
+        domains: ['localhost', 'www.basetime.online', 'staging.basetime.online'],
     },
 };
 

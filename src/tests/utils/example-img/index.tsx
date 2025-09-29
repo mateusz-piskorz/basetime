4cc0c3f4f72b3e443ffb094a84990e8f2e8a658c
epickaowca <epickaowca@Epickas-Laptop.local>
2025-09-29 18:44:31 +0200

    avatar-tests leftovers

--- src/tests/utils/example-img/index.tsx
+++ src/tests/utils/example-img/index.tsx
@@ -4,14 +4,14 @@ import path from 'path';
 export function loadTestNonSharedBuffer() {
     const __dirname = path.dirname(__filename);
 
-    const pngPath = path.join(__dirname, '..', 'example-img', 'example.png');
+    const pngPath = path.join(__dirname, 'example.png');
     return fs.readFileSync(pngPath);
 }
 
 export function loadTestFile() {
     const __dirname = path.dirname(__filename);
 
-    const pngPath = path.join(__dirname, '..', 'example-img', 'example.png');
+    const pngPath = path.join(__dirname, 'example.png');
     const buffer = fs.readFileSync(pngPath);
     return new File([buffer], 'example.png', { type: 'image/png' });
 }

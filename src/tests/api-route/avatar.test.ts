2aa387ade56a5f5d14e12b02c7a6ea65d6b43ec1
epickaowca <epickaowca@Epickas-Laptop.local>
2025-10-02 23:04:54 +0200

    integration-tests-refactor

--- src/tests/api-route/avatar.test.ts
+++ src/tests/api-route/avatar.test.ts
@@ -1,53 +1,49 @@
 import { prisma } from '@/lib/prisma';
 import { encrypt } from '@/lib/utils/encrypt';
-import bcrypt from 'bcrypt';
 import dayjs from 'dayjs';
 import { getStatObject } from '../../lib/minio';
-import { loadTestFile } from '../utils/example-img';
+import { API_URL } from '../utils/constants';
+import { getTestFileFormData } from '../utils/example-img';
 
-const password = 'john1234';
-const userId = 'id123';
-const sessionId = 'id123';
+const userId = 'uid';
+const sessionId = 'sid';
 const expiresAt = dayjs().add(1, 'day').toDate();
+const fileName = `user/${userId}/avatar.png`;
+let headers: HeadersInit;
 
-test('avatar setup', async () => {
-    const pwHash = await bcrypt.hash(password, 9);
-    await prisma.user.create({
-        data: { email: '', name: '', password: pwHash, id: userId, Session: { create: { expiresAt, userAgent: '', id: sessionId } } },
+describe('avatar', () => {
+    beforeAll(async () => {
+        const session = await encrypt({ sessionId, expiresAt });
+        headers = { Cookie: `session=${session}` };
+        await prisma.user.create({
+            data: {
+                email: '',
+                name: '',
+                password: '',
+                id: userId,
+                Session: { create: { expiresAt, userAgent: '', id: sessionId } },
+            },
+        });
     });
 
-    expect(await prisma.user.findUnique({ where: { id: userId } })).not.toBe(null);
-});
+    test('user can update avatar', async () => {
+        const data = await fetch(`${API_URL}/user-avatar`, {
+            method: 'POST',
+            body: getTestFileFormData(),
+            headers,
+        }).then((res) => res.json());
 
-test('user can update avatar', async () => {
-    const img = loadTestFile();
-    const formData = new FormData();
-    formData.set('file', img);
-    const session = await encrypt({ sessionId, expiresAt });
-    const res = await fetch('http://localhost:3000/api/user-avatar', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            Cookie: `session=${session}`,
-        },
+        expect(data.success).toBe(true);
+        expect(await getStatObject({ bucket: 'main', fileName })).not.toBe(undefined);
     });
-    const data = await res.json();
-    expect(data.success).toBe(true);
-    const fileName = `user/${userId}/avatar.png`;
-    expect(await getStatObject({ bucket: 'main', fileName })).not.toBe(undefined);
-});
 
-test('user can delete avatar', async () => {
-    const session = await encrypt({ sessionId, expiresAt });
-    const res = await fetch('http://localhost:3000/api/user-avatar', {
-        method: 'DELETE',
+    test('user can delete avatar', async () => {
+        const data = await fetch(`${API_URL}/user-avatar`, {
+            method: 'DELETE',
+            headers,
+        }).then((res) => res.json());
 
-        headers: {
-            Cookie: `session=${session}`,
-        },
+        expect(data.success).toBe(true);
+        expect(await getStatObject({ bucket: 'main', fileName })).toBe(undefined);
     });
-    const data = await res.json();
-    expect(data.success).toBe(true);
-    const fileName = `user/${userId}/avatar.png`;
-    expect(await getStatObject({ bucket: 'main', fileName })).toBe(undefined);
 });

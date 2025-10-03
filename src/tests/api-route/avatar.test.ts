7bbb383a02800a3a661a9a7480c4735615c30f91
epickaowca <epickaowca@Epickas-Laptop.local>
2025-10-03 11:04:40 +0200

    integration-tests-refactor logo

--- src/tests/api-route/avatar.test.ts
+++ src/tests/api-route/avatar.test.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/lib/prisma';
 import { encrypt } from '@/lib/utils/encrypt';
 import dayjs from 'dayjs';
 import { getStatObject } from '../../lib/minio';
-import { API_URL } from '../utils/constants';
+import { API_BASE_URL } from '../utils/constants';
 import { getTestFileFormData } from '../utils/example-img';
 
 const userId = 'uid';
@@ -10,24 +10,19 @@ const sessionId = 'sid';
 const expiresAt = dayjs().add(1, 'day').toDate();
 const fileName = `user/${userId}/avatar.png`;
 let headers: HeadersInit;
+const apiUrl = `${API_BASE_URL}/user-avatar`;
 
 describe('avatar', () => {
     beforeAll(async () => {
         const session = await encrypt({ sessionId, expiresAt });
         headers = { Cookie: `session=${session}` };
         await prisma.user.create({
-            data: {
-                email: '',
-                name: '',
-                password: '',
-                id: userId,
-                Session: { create: { expiresAt, userAgent: '', id: sessionId } },
-            },
+            data: { email: '', name: '', password: '', id: userId, Session: { create: { expiresAt, userAgent: '', id: sessionId } } },
         });
     });
 
     test('user can update avatar', async () => {
-        const data = await fetch(`${API_URL}/user-avatar`, {
+        const data = await fetch(apiUrl, {
             method: 'POST',
             body: getTestFileFormData(),
             headers,
@@ -38,7 +33,7 @@ describe('avatar', () => {
     });
 
     test('user can delete avatar', async () => {
-        const data = await fetch(`${API_URL}/user-avatar`, {
+        const data = await fetch(apiUrl, {
             method: 'DELETE',
             headers,
         }).then((res) => res.json());
--- src/tests/api-route/logo.test.ts
+++ src/tests/api-route/logo.test.ts
@@ -1,55 +1,95 @@
 import { prisma } from '@/lib/prisma';
 import { encrypt } from '@/lib/utils/encrypt';
-import bcrypt from 'bcrypt';
 import dayjs from 'dayjs';
 import { getStatObject } from '../../lib/minio';
-import { loadTestFile } from '../utils/example-img';
+import { API_BASE_URL } from '../utils/constants';
+import { getTestFileFormData } from '../utils/example-img';
 
-const password = 'john1234';
-const userId = 'id123';
-const sessionId = 'id123';
+const owner = { userId: 'ouid', sessionId: 'osid' };
+const employee = { userId: 'euid', sessionId: 'esid' };
 const expiresAt = dayjs().add(1, 'day').toDate();
-const orgId = 'orgid';
+const orgId = 'oid';
+const fileName = `organization/${orgId}/logo.png`;
+const apiUrl = `${API_BASE_URL}/org/${orgId}/logo`;
+let headersOwner: HeadersInit;
+let headersEmployee: HeadersInit;
 
-test('logo setup', async () => {
-    const pwHash = await bcrypt.hash(password, 9);
-    await prisma.user.create({
-        data: { email: '', name: '', password: pwHash, id: userId, Session: { create: { expiresAt, userAgent: '', id: sessionId } } },
+describe('logo', () => {
+    beforeAll(async () => {
+        const ownerSession = await encrypt({ sessionId: owner.sessionId, expiresAt });
+        const employeeSession = await encrypt({ sessionId: employee.sessionId, expiresAt });
+        headersOwner = { Cookie: `session=${ownerSession}` };
+        headersEmployee = { Cookie: `session=${employeeSession}` };
+
+        await prisma.user.create({
+            data: { email: '1', name: '1', password: '1', id: owner.userId, Session: { create: { expiresAt, userAgent: '', id: owner.sessionId } } },
+        });
+        await prisma.user.create({
+            data: {
+                email: '2',
+                name: '2',
+                password: '2',
+                id: employee.userId,
+                Session: { create: { expiresAt, userAgent: '', id: employee.sessionId } },
+            },
+        });
+
+        await prisma.organization.create({
+            data: {
+                id: orgId,
+                currency: 'EUR',
+                name: '',
+                Members: {
+                    createMany: {
+                        data: [
+                            { role: 'OWNER', userId: owner.userId },
+                            { role: 'EMPLOYEE', userId: employee.userId },
+                        ],
+                    },
+                },
+            },
+        });
     });
 
-    await prisma.organization.create({ data: { id: orgId, currency: 'EUR', name: '', Members: { create: { role: 'OWNER', userId } } } });
+    test('employee cant update logo', async () => {
+        const data = await fetch(apiUrl, {
+            method: 'POST',
+            body: getTestFileFormData(),
+            headers: headersEmployee,
+        }).then((res) => res.json());
 
-    expect(await prisma.user.findUnique({ where: { id: userId } })).not.toBe(null);
-});
-
-test('owner can update logo', async () => {
-    const img = loadTestFile();
-    const formData = new FormData();
-    formData.set('file', img);
-    const session = await encrypt({ sessionId, expiresAt });
-    const res = await fetch(`http://localhost:3000/api/org/${orgId}/logo`, {
-        method: 'POST',
-        body: formData,
-        headers: {
-            Cookie: `session=${session}`,
-        },
+        expect(data.success).toBe(false);
+        expect(await getStatObject({ bucket: 'main', fileName })).toBe(undefined);
     });
-    const data = await res.json();
-    expect(data.success).toBe(true);
-    const fileName = `organization/${orgId}/logo.png`;
-    expect(await getStatObject({ bucket: 'main', fileName })).not.toBe(undefined);
-});
 
-test('owner can delete logo', async () => {
-    const session = await encrypt({ sessionId, expiresAt });
-    const res = await fetch(`http://localhost:3000/api/org/${orgId}/logo`, {
-        method: 'DELETE',
-        headers: {
-            Cookie: `session=${session}`,
-        },
+    test('owner can update logo', async () => {
+        const data = await fetch(apiUrl, {
+            method: 'POST',
+            body: getTestFileFormData(),
+            headers: headersOwner,
+        }).then((res) => res.json());
+
+        expect(data.success).toBe(true);
+        expect(await getStatObject({ bucket: 'main', fileName })).not.toBe(undefined);
     });
-    const data = await res.json();
-    expect(data.success).toBe(true);
-    const fileName = `organization/${orgId}/logo.png`;
-    expect(await getStatObject({ bucket: 'main', fileName })).toBe(undefined);
+
+    test('employee cant delete logo', async () => {
+        const data = await fetch(apiUrl, {
+            method: 'DELETE',
+            headers: headersEmployee,
+        }).then((res) => res.json());
+
+        expect(data.success).toBe(false);
+        expect(await getStatObject({ bucket: 'main', fileName })).not.toBe(undefined);
+    });
+
+    test('owner can delete logo', async () => {
+        const data = await fetch(apiUrl, {
+            method: 'DELETE',
+            headers: headersOwner,
+        }).then((res) => res.json());
+
+        expect(data.success).toBe(true);
+        expect(await getStatObject({ bucket: 'main', fileName })).toBe(undefined);
+    });
 });

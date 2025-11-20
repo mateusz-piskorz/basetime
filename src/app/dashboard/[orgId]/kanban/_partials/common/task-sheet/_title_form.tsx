822bb2bb079d58827cdfd20700f63119ebdcbc6a
epickaowca <epickaowca@Epickas-Laptop.local>
2025-11-20 14:53:17 +0100

    task-card-improvements leftovers

--- src/app/dashboard/[orgId]/kanban/_partials/common/task-sheet/_title_form.tsx
+++ src/app/dashboard/[orgId]/kanban/_partials/common/task-sheet/_title_form.tsx
@@ -48,12 +48,7 @@ export const TitleForm = ({ task }: Props) => {
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 text-sm">
                         <div className="dark:bg-input/30 rounded-md border">
-                            <InputField
-                                autoFocus
-                                form={form}
-                                name="name"
-                                classNameInput="border-none shadow-none max-h-[150px] resize-none dark:bg-transparent"
-                            />
+                            <InputField autoFocus form={form} name="name" classNameInput="border-none shadow-none dark:bg-transparent" />
 
                             <div className="flex justify-end gap-2 p-4">
                                 <Button

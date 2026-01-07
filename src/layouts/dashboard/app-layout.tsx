f786dc4722a49c4d1e540551eb2baf697fbebb82
epickaowca <epickaowca@Epickas-Laptop.local>
2026-01-07 14:40:16 +0100

    nav-progress-bar

--- src/layouts/dashboard/app-layout.tsx
+++ src/layouts/dashboard/app-layout.tsx
@@ -1,10 +1,10 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import NextTopLoader from 'nextjs-toploader';
 import { type ReactNode } from 'react';
 import { AppHeader } from './app-header';
 import { AdminPanelSidebar } from './app-sidebar/admin-panel';
 import { OrganizationSidebar } from './app-sidebar/organization';
 import { UserSidebar } from './app-sidebar/user';
-
 interface AppLayoutProps {
     children: ReactNode;
     type: 'organization' | 'user' | 'admin-panel';
@@ -17,7 +17,7 @@ export const AppLayout = ({ children, type }: AppLayoutProps) => {
 
             <SidebarInset>
                 <AppHeader />
-
+                <NextTopLoader color="hsl(213, 94, 68)" initialPosition={0.08} crawlSpeed={200} height={3} showSpinner={false} />
                 {children}
             </SidebarInset>
         </SidebarProvider>
